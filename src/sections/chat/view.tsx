8a5cb927dbc6561bb8d2557963d3dffef3664033
Arash Rostami <a.rostami2400@gmail.com>
2024-07-30 16:16:55 +0330

    Fix; chat rtl

--- src/sections/chat/view.tsx
+++ src/sections/chat/view.tsx
@@ -5,49 +5,58 @@ import Box from "@mui/material/Box";
 import ReactMarkdown from 'react-markdown'
 import {useSettingsContext} from "../../components/settings";
 
+function classNames(...classes: any) {
+    return classes.filter(Boolean).join(' ')
+}
+
 export default function ChatView() {
     const settings = useSettingsContext();
 
     return (
-        <div className={'w-[100%] h-[90vh] overflow-y-scroll flex flex-col items-center justify-evenly'}>
-            <div className={'w-[90%] h-[75vh] overflow-y-scroll rounded-lg'}>
-                <Box id={'request-message'} className={'w-[100%] flex flex-row-reverse mt-5'}>
+        <div className={'w-[100%]  h-[90vh] overflow-y-scroll flex flex-col items-center justify-evenly'}>
+            <div className={'w-[90%] md:w-[80%] lg:w-[70%] h-[75vh] overflow-y-scroll rounded-lg'}>
+                <Box id={'request-message'}  className={classNames('w-[100%] flex mt-5', settings.themeDirection === 'rtl' ? 'flex-row' : 'flex-row-reverse')}>
                     <Box sx={{bgcolor: (theme) => alpha(theme.palette.grey[600], 0.1)}}
-                         className={'m-2 w-[70%] md:w-[60%] lg:w-[50%] text-start pt-3 pb-3 pr-5 pl-5 rounded-xl'}>Could you
-                        help me plan a relaxing day that focuses on activities for rejuvenation? To start, can you ask
-                        me
-                        what
-                        my favorite forms of relaxation are?
+                         className={'m-2 w-[70%] md:w-[60%] lg:w-[50%] text-start pt-3 pb-3 pr-5 pl-5 rounded-xl'}>
+                        <p dir={'auto'} className={'w-[100%] h-[100%]'}>
+                            Could you
+                            help me plan a relaxing day that focuses on activities for rejuvenation? To start, can you ask
+                            me
+                            what
+                            my favorite forms of relaxation are?
+                        </p>
                     </Box>
                 </Box>
                 <div id={'divider'} className={"mt-3 mb-3"}></div>
-                <Box id={'response-message'} className={'w-[100%] flex flex-row items-start'}>
+                <Box id={'response-message'} className={classNames('w-[100%] flex items-start', settings.themeDirection === 'rtl' ? 'flex-row-reverse' : 'flex-row')}>
                     <Avatar sx={{marginTop: '3px'}}
                             src={settings.themeMode === 'dark' ? "/logo/logo_light.svg" : "/logo/logo_dark.svg"}/>
                     <Box className={'m-2 w-[80%] md:w-[70%] lg:w-[60%]  text-start pb-3 pr-5 pl-4 rounded-xl'}>
-                        <ReactMarkdown>
-                            Sure! What are your favorite forms of relaxation?
-                            Sure! What are your favorite forms of relaxation?
-                            Sure! What are your favorite forms of relaxation?
-                        </ReactMarkdown>
+                        <div className={'w-[100%] h-[100%]'} dir={'auto'}>
+                            <ReactMarkdown>
+                                Sure! What are your favorite forms of relaxation?
+                                Sure! What are your favorite forms of relaxation?
+                                Sure! What are your favorite forms of relaxation?
+                            </ReactMarkdown>
+                        </div>
                     </Box>
                 </Box>
-                <div id={'icons'} className={'w-[100%] ml-14 flex flex-row items-start'}>
-                    <div>
+                <div id={'icons'} className={classNames('w-[100%] ml-14 rtl:pl-14 flex', settings.themeDirection === 'rtl' ? 'flex-row-reverse' : 'flex-row')}>
+                    <div className={'cursor-pointer'}>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
                              stroke="currentColor" className="size-5">
                             <path strokeLinecap="round" strokeLinejoin="round"
                                   d="M19.114 5.636a9 9 0 0 1 0 12.728M16.463 8.288a5.25 5.25 0 0 1 0 7.424M6.75 8.25l4.72-4.72a.75.75 0 0 1 1.28.53v15.88a.75.75 0 0 1-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.009 9.009 0 0 1 2.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75Z"/>
                         </svg>
                     </div>
-                    <div className={'ml-3'}>
+                    <div className={'ml-3 cursor-pointer'}>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
                              stroke="currentColor" className="size-5">
                             <path strokeLinecap="round" strokeLinejoin="round"
                                   d="M15.666 3.888A2.25 2.25 0 0 0 13.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 0 1-.75.75H9a.75.75 0 0 1-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 0 1-2.25 2.25H6.75A2.25 2.25 0 0 1 4.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 0 1 1.927-.184"/>
                         </svg>
                     </div>
-                    <div className={'ml-3'}>
+                    <div className={'ml-3 cursor-pointer'}>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
                              stroke="currentColor" className="size-5">
                             <path strokeLinecap="round" strokeLinejoin="round"
@@ -57,8 +66,8 @@ export default function ChatView() {
                 </div>
             </div>
             <Box sx={{bgcolor: (theme) => alpha(theme.palette.grey[600], 0.1)}}
-                 className="flex items-center justify-center gap-1.5 md:gap-2 w-[90%] mr-2 ml-2 rounded-full">
-                <div className="flex flex-col justify-center cursor-pointer items-center ml-4 mr-2">
+                 className="flex items-center justify-center gap-1.5 md:gap-2 w-[90%] md:w-[80%] lg:w-[70%] mr-2 ml-2 rounded-full">
+                <div className="flex flex-col justify-center cursor-pointer items-center rtl:ml-2 rtl:mr-4 ltr:ml-4 ltr:mr-2">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
                          stroke="currentColor" className="size-6">
                         <path strokeLinecap="round" strokeLinejoin="round"
@@ -66,15 +75,16 @@ export default function ChatView() {
                     </svg>
                 </div>
                 <div className="flex min-w-0 flex-1 flex-col items-start justify-end">
-                    <textarea dir="auto"  placeholder="Message Intellectera"
+                    <textarea dir="auto" placeholder={'Message Intellectera'}
                               className="m-0 w-[100%] place-content-center text-lg resize-none border-0 bg-transparent px-0 text-token-text-primary focus:ring-0 focus-visible:ring-0 max-h-52"
                     ></textarea>
                 </div>
-                <button className={'flex flex-col justify-center items-center cursor-pointer ml-2 mr-4'}>
+                <button
+                    className={'flex flex-col justify-center items-center cursor-pointer ltr:ml-2 ltr:mr-4 rtl:ml-4 rtl:mr-2'}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
                          stroke="currentColor" className="size-6">
                         <path strokeLinecap="round" strokeLinejoin="round"
-                              d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5"/>
+                              d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5"/>
                     </svg>
                 </button>
             </Box>
